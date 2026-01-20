3b5a94a1644a9cb1ecfa364e9eb795b7d87ed8af
Cody <chal032687@gmail.com>
2026-01-20 12:42:39 -0500

    file-type.js and wrangler.inquisitor.jsonc updates

--- src/worker/shims/file-type.js
+++ src/worker/shims/file-type.js
@@ -1,3 +1,4 @@
 export const fileTypeFromFile = async () => null
 export const fileTypeFromBuffer = async () => null
-export default { fileTypeFromFile, fileTypeFromBuffer }
+export const fileTypeFromStream = async () => null
+export const fileTypeFromTokenizer = async () => null
